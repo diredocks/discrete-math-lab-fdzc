881f3637f7dbaf65bacde11547a26bf5f2517af3
diredocks <26994007+diredocks@users.noreply.github.com>
2025-03-19 20:28:39 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A Nix-flake-based Python development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          venvDir = ".venv";
+          shellHook = ''
+            export SHELL=$(which zsh)
+          '';
+          packages = with pkgs; [ python312 ] ++
+            (with pkgs.python312Packages; [
+              pip
+              numpy
+              ruff
+              venvShellHook
+            ]);
+        };
+      });
+    };
+}
