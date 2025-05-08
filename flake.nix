4b1d9b462ec2685a94d6cdad6e11c0827283c90f
diredocks <26994007+diredocks@users.noreply.github.com>
2025-05-08 17:32:08 +0800

    chore: fmt code

--- flake.nix
+++ flake.nix
@@ -3,28 +3,32 @@
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          venvDir = ".venv";
-          shellHook = ''
-            export SHELL=$(which zsh)
-          '';
-          packages = with pkgs; [ python312 ] ++
-            (with pkgs.python312Packages; [
-              pip
-              ruff
-              pyright
-              venvShellHook
-            ]);
-        };
-      });
-    };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        venvDir = ".venv";
+        shellHook = ''
+          export SHELL=$(which zsh)
+        '';
+        packages = with pkgs;
+          [python312]
+          ++ (with pkgs.python312Packages; [
+            pip
+            ruff
+            pyright
+            venvShellHook
+          ]);
+      };
+    });
+  };
 }
